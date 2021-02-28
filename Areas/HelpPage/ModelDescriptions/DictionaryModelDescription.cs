c9cb4a26cb27e2f603a848104c17744c46af73e8
jot123971 <kaurjyoti67871@gmail.com>
2021-03-01 11:26:06 +1300

    Add project files.

---
+++ Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ProductSoreWebAPICRUD.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProductSoreWebAPICRUD.Models
+{
+    public class Product
+    {
+        // Primary Key Setup
+        [Key]
+        public int Id { get; set; }
+        // property stores the name of the product
+        public string Name { get; set; }
+        // property stores the category of the product
+        public string Category { get; set; }
+        // stores the product stock status
+        public string StockStatus { get; set; }
+        // this stores the price of the product
+        public decimal Price { get; set; }
+    
+    }
+}
